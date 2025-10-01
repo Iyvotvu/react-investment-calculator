8c64542e5c279fc463783ac854555538a660a3c1
Iyvotvu <jvrarmand@gmail.com>
2025-10-01 17:44:22 +0200

    IC-1: - Quick little project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import {useState} from "react";
+import Results from "./components/Results.jsx";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 0,
+        annualInvestment: 0,
+        expectedReturn: 0,
+        duration: 1,
+    });
+
+    const inputIsValid = userInput.duration >= 1;
+
+    function handleInputChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: +newValue
+            };
+        });
+    }
+
+    return (
+        <>
+            <Header/>
+            <UserInput userInput={userInput} onChange={handleInputChange}/>
+            {!inputIsValid && <p className="center">Please enter a valid duration</p>}
+            {inputIsValid && <Results userInput={userInput}/>}
+        </>
+    )
 }
 
 export default App
